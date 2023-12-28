78554c4df1f661f03ac1588cfedb89bfe399b54c
mohammad <ronins1375@gmail.com>
2023-12-28 13:39:27 +0330

    first

--- ui.js
+++ ui.js
@@ -1,9 +1,34 @@
 const [todoList] = document.getElementsByClassName('list');
+const input = document.getElementById('input')
+const btnSave = document.getElementById('btnSave')
+const item = document.querySelectorAll('.item')
+const data = []
+btnSave.addEventListener('click', () => data.push(input.value))
+btnSave.addEventListener('click', Todo)
 
-
-const Todo = (text) => {
-  const html = `<li class='item'>${text}</li>`
-  const li = document.createElement('li');
-  li.innerHTML = html; 
-  return li;
+function Todo() {
+  const text = input.value
+  if (text.trim() !== '') {
+    console.log(data)
+    const html = ` ${text}
+ <button   onclick="deleteLi(this)"> delete</button> `
+    const li = document.createElement('li');
+    li.innerHTML = html;
+    li.classList = "item"
+    todoList.appendChild(li)
+    input.value = ''
+    console.log(li)
+  }
 }
+
+function gettingBtn() {
+  item.map((li) => { li.childeren[0].addEventListener('click', (btn) => btn.parentElement.remove()) })
+}
+function deleteLi(btn) {
+  btn.parentElement.remove()
+  data = data.filter((a) => a !== btn.value)
+}
+
+
+
+
